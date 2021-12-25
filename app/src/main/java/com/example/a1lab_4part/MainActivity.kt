6dfa4d9cba489a369e70ca30c95dd89b9749cda6
MarinaLipatnikova <mlipatnikova2000@mail.ru>
2021-12-25 16:14:29 +0300

    1

--- app/src/main/java/com/example/a1lab_4part/MainActivity.kt
+++ app/src/main/java/com/example/a1lab_4part/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.a1lab_4part
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.TextView
+import android.widget.Button
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val text: TextView = findViewById(R.id.text)
+        val but: Button = findViewById(R.id.button2)
+        but.setOnClickListener {
+            text.text = "Goodbye world :("
+        }
     }
 }
+
